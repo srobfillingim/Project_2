97af0ac907594061fcd65c016f8726045953087f
nate pearson <nate913@gmail.com>
2019-06-06 14:04:43 -0500

    added seeds and added sequelize stuff to roadtripjs

--- models/roadTrip.js
+++ models/roadTrip.js
@@ -1,7 +1,21 @@
-module.exports = function(sequelize, DataTypes) {
-  var RoadTrip = sequelize.define("RoadTrip", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
-  });
-  return RoadTrip;
-};
+// Dependencies
+// =============================================================
+
+// Sequelize (capital) references the standard library
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references my connection to the DB.
+var sequelize = require("../config/connection.js");
+
+// Creates a "Book" model that matches up with DB
+var roadTrip = sequelize.define("roadTrip", {
+  city: Sequelize.STRING,
+  state: Sequelize.STRING,
+  activity: Sequelize.STRING,
+  weather: Sequelize.INTEGER
+});
+
+// Syncs with DB
+roadTrip.sync();
+
+// Makes the Book Model available for other files (will also create a table)
+module.exports = roadTrip;
---
+++ models/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO states (states) values ('Illinois');
+INSERT INTO states (States) values ('Missouri');
+INSERT INTO states (States) values ('Kansas');
+INSERT INTO states (States) values ('Oklahoma');
+INSERT INTO states (States) values ('Texas');
+INSERT INTO states (States) values ('New Mexico');
+INSERT INTO states (States) values ('Arizona');
+INSERT INTO states (States) values ('California');
+
+INSERT INTO city (city, stateId) values ('Chicago',1);
+INSERT INTO city (city, stateId) values ('St.Louis',2);
+INSERT INTO city (city, stateId) values ('Baxter Springs',3);
+INSERT INTO city (city, stateId) values ('Oaklahoma City',4);
+INSERT INTO city (city, stateId) values ('Shamrock',5);
+INSERT INTO city (city, stateId) values ('Albuquerque',6);
+INSERT INTO city (city, stateId) values ('Flagstaff',7);
+INSERT INTO city (city, stateId) values ('Pomona',8);
