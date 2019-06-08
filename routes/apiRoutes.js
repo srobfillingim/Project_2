3310a78407d59d62c09c76ca663f72c7b5a9c6d8
Samuel Fillingim <srobfillingim@gmail.com>
2019-06-08 11:50:01 -0500

    spacing and typos

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,25 @@
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Get all roadTrips
-  app.get("/api/roadTrips", function (req, res) {
-    db.RoadTrips.findAll({}).then(function (dbRoadTrips) {
+  app.get("/api/roadTrips", function(req, res) {
+    db.RoadTrips.findAll({}).then(function(dbRoadTrips) {
       res.json(dbRoadTrips);
     });
   });
 
   // Create a new roadTrip
-  app.post("/api/roadTrips", function (req, res) {
-    db.RoadTrip.create(req.body).then(function (dbRoadTrip) {
+  app.post("/api/roadTrips", function(req, res) {
+    db.RoadTrip.create(req.body).then(function(dbRoadTrip) {
       res.json(dbRoadTrip);
     });
   });
 
   // Delete a roadTrip by id
-  app.delete("/api/roadTrips/:id", function (req, res) {
-    db.RoadTrip.destroy({ where: { id: req.params.id } }).then(function (dbRoadTrip) {
+  app.delete("/api/roadTrips/:id", function(req, res) {
+    db.RoadTrip.destroy({ where: { id: req.params.id } }).then(function(
+      dbRoadTrip
+    ) {
       res.json(dbRoadTrip);
     });
   });
--- test/canary.test.js
+++ test/canary.test.js
@@ -1,9 +1,70 @@
-var expect = require("chai").expect;
+// var expect = require("chai").expect;
 
-describe("canary test", function() {
-  // A "canary" test is one we set up to always pass
-  // This can help us ensure our testing suite is set up correctly before writing real tests
-  it("should pass this canary test", function() {
-    expect(true).to.be.true;
+// describe("canary test", function() {
+//   // A "canary" test is one we set up to always pass
+//   // This can help us ensure our testing suite is set up correctly before writing real tests
+//   it("should pass this canary test", function() {
+//     expect(true).to.be.true;
+//   });
+// });
+
+var chai = require("chai");
+var chaiHttp = require("chai-http");
+var server = require("../server");
+var db = require("../models");
+var expect = chai.expect;
+
+// Setting up the chai http plugin
+chai.use(chaiHttp);
+
+var request;
+
+describe("GET /api/roadTrips", function() {
+  // Before each test begins, create a new request server for testing
+  // & delete all examples from the db
+  beforeEach(function() {
+    request = chai.request(server);
+    return db.sequelize.sync({ force: true });
+  });
+
+  it("should find all roadTrips", function(done) {
+    // Add some roadTrips to the db to test with
+    db.RoadTrip.bulkCreate([
+      { text: "First RoadTrip", description: "First Description" },
+      { text: "Second RoadTrip", description: "Second Description" }
+    ]).then(function() {
+      // Request the route that returns all examples
+      request.get("/api/roadTrips").end(function(err, res) {
+        var responseStatus = res.status;
+        var responseBody = res.body;
+
+        // Run assertions on the response
+
+        expect(err).to.be.null;
+
+        expect(responseStatus).to.equal(200);
+
+        expect(responseBody)
+          .to.be.an("array")
+          .that.has.lengthOf(2);
+
+        expect(responseBody[0])
+          .to.be.an("object")
+          .that.includes({
+            text: "First roadTrip",
+            description: "First Description"
+          });
+
+        expect(responseBody[1])
+          .to.be.an("object")
+          .that.includes({
+            text: "Second roadTrip",
+            description: "Second Description"
+          });
+
+        // The `done` function is used to end any asynchronous tests
+        done();
+      });
+    });
   });
 });
